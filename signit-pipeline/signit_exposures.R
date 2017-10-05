bce19eb68503f6f2ba34a52ccaae107df28d791f
Eric Zhao <ezhao@gphost04.bcgsc.ca>
2017-10-05 16:25:47 -0700

    Added SignIT pipeline running scripts

---
+++ signit-pipeline/signit_exposures.R
@@ -0,0 +1,36 @@
+' signit_exposures.R
+
+Usage: signit_exposures.R -c CATALOG -o OUTPUT [ -s SIGNIT ]
+
+Options:
+    -c --catalog CATALOG        Path to mutation catalog. TSV with two columns: mutation_type (str) and count (int).
+
+    -o --output OUTPUT          Path to .Rds file containing the serialized exposures object
+
+    -s --signit SIGNIT          Path to SignIT R library files. If not provided, will assume that SignIT is installed
+                                and load the package using library(signit) instead.
+' -> doc
+
+library(docopt)
+args <- docopt(doc)
+
+if (is.null(args[['signit']])) {
+    library(signit)
+} else {
+    library(devtools)
+    library(tidyverse)
+    library(rjags)
+    library(nnls)
+    library(dbscan)
+    library(Rtsne)
+
+    load_all(args[['signit']])
+}
+
+exposures <- get_exposures(file = args[['catalog']])
+
+print('SignIT Analysis Complete.')
+
+saveRDS(exposures, file = args[['output']])
+
+print(paste0('Results saved as RDS serialized object in ', args[['output']]))
